f85cfb20f2051c562b57be62c432dca7e1235f07
alexanderbeletsky <alexander.beletsky@gmail.com>
2014-03-16 22:10:08 +0200

    users api spec draft

--- test/specs/users.spec.js
+++ test/specs/users.spec.js
@@ -1,5 +1,57 @@
 describe('users.spec.js', function () {
-	it('should fail', function () {
-		expect(1).to.equal(0);
+
+	describe('when creating users', function () {
+		describe('when user created', function () {
+
+			it('should be created', function () {
+
+			});
+
+			it('should have lastImpressionAt', function () {
+
+			});
+
+			it('should have session count as one', function () {
+
+			});
+		});
+
+		describe('when user updated', function () {
+			it('should be updated', function () {
+
+			});
+
+			it('should have update lastImpressionAt', function () {
+
+			});
+
+			it('should have session count as one', function () {
+
+			});
+
+			describe('after half of hour', function () {
+				beforeEach(function () {
+					// hack lastImpressionAt in db
+				});
+
+				it('should session incremented', function () {
+
+				});
+			});
+		});
+	});
+
+	describe('when getting users', function () {
+		describe('if empty', function () {
+
+		});
+
+		describe('if for one page', function () {
+
+		});
+
+		describe('if for two pages', function () {
+
+		});
 	});
 });
