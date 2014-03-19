7ef5d2b7338aec38e1ebb62cffc772ac4f11aab3
alexanderbeletsky <alexander.beletsky@gmail.com>
2014-03-19 12:12:52 +0200

    base.html added

--- source/site.js
+++ source/site.js
@@ -1,14 +1,23 @@
 var http = require('http');
 var express = require('express');
+var swig = require('swig');
 
 var app = express();
 
 app.configure(function(){
 	app.set('port', process.env.PORT || 5004);
-	app.use(express.bodyParser());
+	app.engine('html', swig.renderFile);
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'html');
 	app.use(express.cookieParser());
+	app.use(express.json());
+	app.use(express.urlencoded());
 	app.use(express.methodOverride());
 	app.use(app.router);
+});
+
+app.get('/', function (req, res) {
+	res.render('dashboard');
 });
 
 http.createServer(app).listen(app.get('port'), function (err) {
