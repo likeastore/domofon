35568455345606a33b3a4d1a63f932dc50603cc6
alexanderbeletsky <alexander.beletsky@gmail.com>
2014-03-19 12:24:04 +0200

    master page served

--- source/site.js
+++ source/site.js
@@ -1,13 +1,15 @@
 var http = require('http');
 var express = require('express');
 var swig = require('swig');
+var path = require('path');
 
 var app = express();
+var env = process.env.NODE_ENV || 'development';
 
 app.configure(function(){
 	app.set('port', process.env.PORT || 5004);
 	app.engine('html', swig.renderFile);
-	app.set('views', __dirname + '/views');
+	app.set('views', __dirname + '/../views');
 	app.set('view engine', 'html');
 	app.use(express.cookieParser());
 	app.use(express.json());
@@ -16,11 +18,39 @@ app.configure(function(){
 	app.use(app.router);
 });
 
+app.configure('development', function() {
+	app.set('view cache', false);
+	swig.setDefaults({ cache: false });
+	app.use(express.logger('dev'));
+	app.use(express.static(path.join(__dirname, '/../public')));
+	app.use(express.errorHandler());
+});
+
+app.configure('staging', function () {
+	app.set('view cache', false);
+	swig.setDefaults({ cache: false });
+	//app.use(express.basicAuth(config.access.user, config.access.password));
+	app.use(express.logger('short'));
+	app.use(express.compress());
+	app.use(express.static(path.join(__dirname, '/../public')));
+});
+
+app.configure('production', function() {
+	app.use(express.logger('short'));
+	app.use(express.compress());
+	app.use(express.static(path.join(__dirname, '/../public')));
+});
+
+app.configure('test', function() {
+	app.use(express.static(path.join(__dirname, '/../public')));
+	app.use(express.errorHandler());
+});
+
+
 app.get('/', function (req, res) {
-	res.render('dashboard');
+	res.render('dashboard', {title: 'Domofon dashboard', mode: env});
 });
 
 http.createServer(app).listen(app.get('port'), function (err) {
-	var env = process.env.NODE_ENV || 'development';
 	console.log('domofon site started on port ' + app.get('port') + ' env ' + env);
 });
